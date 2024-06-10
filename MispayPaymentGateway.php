eb3dab2e31d40b9cfc806cb1326e6d878aff314e
Ferhat Yalçın <=>
2024-06-10 14:37:11 +0300

    update for linting

--- MispayPaymentGateway.php
+++ MispayPaymentGateway.php
@@ -34,7 +34,7 @@ class WC_MisPay extends WC_Payment_Gateway
         $this->init_settings();
 
         $isArabic = (get_locale() === 'ar' || get_locale() === 'ar_SA');
-        
+
         $this->title = $isArabic ? $this->get_option('titleAR') : $this->get_option('titleEN');
         $this->description = $isArabic ? $this->get_option('descriptionAR') : $this->get_option('descriptionEN');
         $this->enabled = $this->get_option('enabled');
